48b1f9c8f65976550ce3c2ddf960dbf6258e87ce
Elanis <elanis@hotmail.com>
2025-03-15 10:38:07 +0100

    Prevent spamming steamwebapi from Lumen

--- Lumen.Modules.SteamPartner.Module/Controllers/SteamDataController.cs
+++ Lumen.Modules.SteamPartner.Module/Controllers/SteamDataController.cs
@@ -9,12 +9,21 @@ namespace Lumen.Modules.SteamPartner.Module.Controllers {
     [ApiController]
     [Route("[controller]")]
     public class SteamDataController(ILogger<SteamDataController> logger, ISteamPartnerStats steamPartnerStats) : ControllerBase {
+        private static DateTime? LastRun = null;
+
         [HttpPost("queryDataFromSteam")]
         public async Task<IActionResult> QueryDataFromSteam([FromBody] SteamToken steamToken) {
+            if (LastRun is not null && LastRun.Value.AddHours(1) > DateTime.UtcNow) {
+                logger.LogInformation("Skipping QueryDataFromSteam() to prevent spamming Steam API");
+                return NoContent();
+            }
+
             logger.BeginScope("Getting data from the Steam API ...");
 
             try {
                 await steamPartnerStats.QuerySaleAndWishlistDataAsync(steamToken.SteamLoginSecure);
+
+                LastRun = DateTime.UtcNow;
 
                 return NoContent();
             } catch (Exception ex) {

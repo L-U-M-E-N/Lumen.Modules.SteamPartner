e1f1aa66effbbe910deb62c4f8a2c33cd21b23e9
Elanis <elanis@hotmail.com>
2025-03-15 10:22:49 +0100

    Add Tampermonkey script to the repository

---
+++ tampermonkey-script/script.js
@@ -0,0 +1,38 @@
+// ==UserScript==
+// @name         Wishlists & Sale LUMEN
+// @namespace    http://elanis.eu/
+// @version      2025-03-15
+// @description  Sends the current session cookie to the Lumen API's SteamPartner Module, so it can query sales and wishlist data and save them in database for later reporting
+// @author       Axel "Elanis" Soupé
+// @match        https://partner.steampowered.com/*
+// @icon         const LUMEN_SERVER_AUTH_HEADER_NAME = 'X-Api-Key';
+// @grant        GM.cookie
+// @grant        GM_xmlhttpRequest
+// @homepageURL  https://github.com/L-U-M-E-N/Lumen.Modules.SteamPartner
+// ==/UserScript==
+
+////////////////
+// CONFIG
+////////////////
+const LUMEN_SERVER_URL = 'FILLME';
+const LUMEN_SERVER_API_KEY = 'FILLME';
+
+//// Constants
+const LUMEN_SERVER_AUTH_HEADER_NAME = 'X-Api-Key';
+const LUMEN_SERVER_ROUTE = 'SteamData/queryDataFromSteam';
+const STEAM_PARTNERS_URL = 'https://partner.steampowered.com/';
+const STEAM_PARTNERS_COOKIE_NAME = 'steamLoginSecure';
+
+//// Execution
+const cs = await GM.cookie.list({ url: STEAM_PARTNERS_URL, partitionKey: {} });
+const cookie = cs.find((x) => x.name === STEAM_PARTNERS_COOKIE_NAME);
+console.log(cookie);
+GM.xmlHttpRequest({
+  url: LUMEN_SERVER_URL + LUMEN_SERVER_ROUTE,
+  method: "POST",
+  data: JSON.stringify({ [STEAM_PARTNERS_COOKIE_NAME]: cookie.value }),
+  headers: {
+    "Content-Type": "application/json",
+    [LUMEN_SERVER_AUTH_HEADER_NAME]: LUMEN_SERVER_API_KEY
+  },
+}).then(o => console.log(o)).catch(e => console.error(e));

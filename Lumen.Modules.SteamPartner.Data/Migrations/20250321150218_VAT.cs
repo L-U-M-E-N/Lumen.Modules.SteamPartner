dcbd8d443bfc05d012f58f3ce5a58679f26c8bb2
Elanis <elanis@hotmail.com>
2025-03-21 16:03:09 +0100

    Create migration for the previous update

---
+++ Lumen.Modules.SteamPartner.Data/Migrations/20250321150218_VAT.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lumen.Modules.SteamPartner.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class VAT : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "USDVATTaxes",
+                schema: "steampartner",
+                table: "PackagesSales",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "USDVATTaxes",
+                schema: "steampartner",
+                table: "PackagesSales");
+        }
+    }
+}

4a89e01ce70b260d752095a66b6a158515c15445
Elanis <elanis@hotmail.com>
2025-09-14 11:18:12 +0200

    Update dependencies

--- Lumen.Modules.SteamPartner.Module/SteamPartnerModule.cs
+++ Lumen.Modules.SteamPartner.Module/SteamPartnerModule.cs
@@ -8,10 +8,10 @@ using Lumen.Modules.SteamPartner.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace Lumen.Modules.SteamPartner.Module {
-	public class SteamPartnerModule(IEnumerable<ConfigEntry> configEntries, ILogger<LumenModuleBase> logger, IServiceProvider provider) : LumenModuleBase(configEntries, logger, provider) {
-		public override async Task InitAsync(LumenModuleRunsOnFlag currentEnv) {
-			await RunAsync(currentEnv, DateTime.UtcNow);
-		}
+    public class SteamPartnerModule(IEnumerable<ConfigEntry> configEntries, ILogger<LumenModuleBase> logger, IServiceProvider provider) : LumenModuleBase(configEntries, logger, provider) {
+        public override async Task InitAsync(LumenModuleRunsOnFlag currentEnv) {
+            await RunAsync(currentEnv, DateTime.UtcNow);
+        }
 
         public override async Task RunAsync(LumenModuleRunsOnFlag currentEnv, DateTime date) {
             try {
@@ -23,27 +23,27 @@ namespace Lumen.Modules.SteamPartner.Module {
             }
         }
 
-		public override bool ShouldRunNow(LumenModuleRunsOnFlag currentEnv, DateTime date) {
-			return currentEnv == LumenModuleRunsOnFlag.API && date.Second == 0 && date.Minute == 0 && date.Hour == 6;
-		}
+        public override bool ShouldRunNow(LumenModuleRunsOnFlag currentEnv, DateTime date) {
+            return currentEnv == LumenModuleRunsOnFlag.API && date.Second == 0 && date.Minute == 0 && date.Hour == 6;
+        }
 
-		public override Task ShutdownAsync() {
-			// Nothing to do
-			return Task.CompletedTask;
-		}
+        public override Task ShutdownAsync() {
+            // Nothing to do
+            return Task.CompletedTask;
+        }
 
-		public static new void SetupServices(LumenModuleRunsOnFlag currentEnv, IServiceCollection serviceCollection, string? postgresConnectionString) {
-			if (currentEnv == LumenModuleRunsOnFlag.API) {
-				serviceCollection.AddDbContext<SteamPartnerContext>(o => o.UseNpgsql(postgresConnectionString, x => x.MigrationsHistoryTable("__EFMigrationsHistory", SteamPartnerContext.SCHEMA_NAME)));
-				serviceCollection.AddTransient<ISteamPartnerStats, SteamPartnerStats>();
-				serviceCollection.AddTransient<ISteamCommunity, SteamCommunity>();
-				serviceCollection.AddTransient<ISteamPartner, Dysnomia.Common.SteamWebAPI.SteamPartner>();
-				serviceCollection.AddHttpClient();
-			}
-		}
+        public static new void SetupServices(LumenModuleRunsOnFlag currentEnv, IServiceCollection serviceCollection, string? postgresConnectionString) {
+            if (currentEnv == LumenModuleRunsOnFlag.API) {
+                serviceCollection.AddDbContext<SteamPartnerContext>(o => o.UseNpgsql(postgresConnectionString, x => x.MigrationsHistoryTable("__EFMigrationsHistory", SteamPartnerContext.SCHEMA_NAME)));
+                serviceCollection.AddTransient<ISteamPartnerStats, SteamPartnerStats>();
+                serviceCollection.AddTransient<ISteamCommunity, SteamCommunity>();
+                serviceCollection.AddTransient<ISteamPartner, Dysnomia.Common.SteamWebAPI.SteamPartner>();
+                serviceCollection.AddHttpClient();
+            }
+        }
 
-		public override Type GetDatabaseContextType() {
-			return typeof(SteamPartnerContext);
-		}
-	}
+        public override Type GetDatabaseContextType() {
+            return typeof(SteamPartnerContext);
+        }
+    }
 }
